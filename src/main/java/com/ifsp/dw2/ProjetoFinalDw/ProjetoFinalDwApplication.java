9437f87231e391f93a0445eee49b8fa64457cbbe
idd_kcorrea <idd_kcorrea@uolinc.com>
2021-12-27 20:31:47 -0300

    inciando

---
+++ src/main/java/com/ifsp/dw2/ProjetoFinalDw/ProjetoFinalDwApplication.java
@@ -0,0 +1,13 @@
+package com.ifsp.dw2.ProjetoFinalDw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalDwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalDwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifsp/dw2/ProjetoFinalDw/ProjetoFinalDwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsp.dw2.ProjetoFinalDw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalDwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
